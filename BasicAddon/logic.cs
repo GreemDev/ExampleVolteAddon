d55e5d19bc9f6f88c5979a1d385a51baf25315a5
Evan Husted <gr33m11@gmail.com>
2021-10-01 05:00:52 -0500

    if i notice another mistake im gonna go ballistic

--- BasicAddon/logic.cs
+++ BasicAddon/logic.cs
@@ -1,6 +1,6 @@
 Client.MessageReceived += async socketMessage => 
 {
-  if (m.ShouldHandle(out var userMessage) && userMessage.Content.Contains("ping")) 
+  if (socketMessage.ShouldHandle(out var userMessage) && userMessage.Content.Contains("ping")) 
   {
     await userMessage.ReplyAsync("pong!");
   }
